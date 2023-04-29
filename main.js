bdcd7a80c22eb05ad02abe670f72d6cf5e380625
maria Anosova <maria.anosova@bellevuecollege.edu>
2023-04-28 20:58:12 -0700

    Adding input into the array

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
+// document.addEventListener("DOMContentLoaded", function (event) {
+
+//     document.getElementById("getResult").addEventListener("click", function() {
+        
+//         foodArray = [];
+//         let Food = {
+//             name: document.getElementById("fname").value,
+//             calories: document.getElementById("numCalories").value
+//         }
+//         document.getElementById("fname").value = "";
+//         document.getElementById("numCalories").value = "";
+
+//         foodArray.push(Food);
+//         console.log(foodArray);
+//     });
+// });
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    document.getElementById("getResult").addEventListener("click", function() {
 
-        document.getElementById("textbox1").value = "success";
+        let Food = function (pName, pCalories) {
+            this.name = pName;
+            this.calories = pCalories;
+        }
+                
+        let food1 = new Food ("apple", 54);
+        let food2 = new Food ("Noodle soup", 325);
+
+        let food3 = new Food (document.getElementById("fname").value, document.getElementById("numCalories").value);          
+        document.getElementById("fname").value = "";
+        document.getElementById("numCalories").value = "";
+        console.log(food3);
     });
 });
+
+
+
+
 
